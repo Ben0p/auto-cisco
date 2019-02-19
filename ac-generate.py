4a8299812467abb10fa0ee40e3ccf0ceae5787bd
Ben Gorham <bgorham@fmgl.com.au>
2019-02-19 21:04:36 +0800

    lists and generate

--- ac-generate.py
+++ ac-generate.py
@@ -1,6 +1,64 @@
+import os
+import time
+
+'''
+Generates folders and config files for each device in lists
+'''
+
+
+<<<<<<< HEAD
+files = [
+    ['./configs/lvs', './lists/out/lvs.csv'],
+    ['./configs/trucks', './lists/out/trucks.csv'],
+    ['./configs/cmd', './lists/out/cmd.csv'],
+    ['./configs/terrain', './lists/out/terrain.csv'],
+    ['./configs/support', './lists/out/support.csv']
+]
+
+
+fleet_config =  '''{}'''.format(open('./configs/fleet', 'r').read())
+
+def openConfig(config_in):
+    with open(config_in) as ci:
+        config = ci.read()
+        ci.close()
+    return(config)
+
+def main():
+    cmd_config = openConfig('./configs/command')
+    fleet_config = openConfig('./configs/fleet')
+    for _file in files:
+        if not os.path.exists(_file[0]):
+            os.makedirs(_file[0])
+            print('Created directory {}'.format(_file[0]))
+        with open(_file[1], 'r') as fi:
+            for line in fi:
+                name, ip = line.split(',')
+                folder_out = '{}/{}'.format(_file[0],name)
+                fleet_file = '{}/{}-fleet.txt'.format(folder_out, name)
+                cmd_file = '{}/{}-command.txt'.format(folder_out, name)
+                if not os.path.exists(folder_out):
+                    os.makedirs(folder_out)
+                    print('Created directory for {}'.format(name))
+                fleet_hostname = '{}-fleet'.format(name)
+                cmd_hostname = '{}-command'.format(name)
+                new_fleet = fleet_config.format(hostname=fleet_hostname, ip=ip)
+                new_cmd = cmd_config.format(hostname=cmd_hostname, ip=ip)
+                with open(fleet_file, 'w') as ff:
+                    ff.write(new_fleet)
+                    ff.close()
+                    print('Created fleet config for {}'.format(name))
+                with open(cmd_file, 'w') as cf:
+                    cf.write(new_cmd)
+                    cf.close()
+                    print('Created fleet config for {}'.format(name))
 
 
 
+if __name__ == '__main__':
+    main()
+=======
 '''
 Generates folders and config files for each device from an input .csv
 '''
+>>>>>>> 01e41dc0a26a437aaa67ab0735663f68ce2f2052
---
+++ ac-lists.py
@@ -0,0 +1,62 @@
+
+
+files = [
+    ['./lists/in/lvs-in.csv', './lists/out/lvs.csv'],
+    ['./lists/in/trucks-in.csv', './lists/out/trucks.csv'],
+    ['./lists/in/cmd-in.csv', './lists/out/cmd.csv'],
+    ['./lists/in/terrain-in.csv', './lists/out/terrain.csv'],
+    ['./lists/in/support-in.csv', './lists/out/support.csv']
+]
+
+fleet_file = ['./lists/in/fleet-in.csv', './lists/out/fleet.csv']
+
+master_list = './lists/master.csv'
+
+def main():
+
+    for _file in files:
+        file_in = _file[0]
+        file_out = _file[1]
+
+        with open(file_out, 'w') as fo:
+
+            with open(file_in, 'r') as fi:
+                for line in fi:
+                    split_line = line.split()
+
+                    if len(split_line) == 5:
+                        name = split_line[0]
+                        ip = split_line[1]
+                    
+                        line_out = '{},{}\n'.format(name,ip)
+
+                        fo.write(line_out)
+                        print(line_out)
+                fi.close()
+            fo.close()
+        
+    with open(fleet_file[1], 'w') as ffo:
+        with open(fleet_file[0], 'r') as ffi:
+            for line in ffi:
+                split_line = line.split()
+                if len(split_line) > 5:
+                    ip = split_line[-2]
+                    name = split_line[-6]
+                    line_out = '{},{}\n'.format(name,ip)
+                    ffo.write(line_out)
+                    print(line_out)
+        ffi.close()
+    ffo.close()
+    
+    with open(master_list 'w') as ml:
+        for 
+
+        # need to match command ip name with fleet name and append into master list
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
